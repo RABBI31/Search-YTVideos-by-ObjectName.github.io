ad7bc0243f6fd374417763166c1f4d575d47217e
Rabbi <alirabbi3112@gmail.com>
2021-05-05 00:52:03 +0600

    Creating Search bar

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Searchbar from './Searchbar'
+
+class App extends React.Component{
+    render() {
+        return(
+            <div><Searchbar /></div>
+        )
+    }
+}
+export default App
---
+++ src/components/Searchbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Searchbar extends React.Component{
+    render(){
+        return(
+            <div className = "search-bar ui segment">
+                <form className="ui form">
+                    <div className = "field">
+                        <label>Video Search</label>
+                        <input type = "text" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+export default Searchbar
